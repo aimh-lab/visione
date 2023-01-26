554c1aeafb7289af66890878bdfb5628d8a1b0a7
Fabio Carrara <fabio.carrara@isti.cnr.it>
2023-01-26 17:27:07 +0100

    commons: added hdf5 saver

--- common/savers.py
+++ common/savers.py
@@ -5,6 +5,7 @@ import logging
 from pathlib import Path
 import sys
 
+import h5py
 from pymongo import MongoClient, UpdateOne
 from pymongo.errors import BulkWriteError
 
@@ -18,7 +19,7 @@ class Saver(abc.ABC):
     @abc.abstractmethod
     def add(self, record):
         pass
-    
+
     def add_many(self, records):
         for record in records:
             self.add(record)
@@ -37,7 +38,7 @@ class GzipJsonpFile(Saver):
             with gzip.open(str(self.path), 'r') as f:
                 self._ids = {json.loads(line)['_id'] for line in f.read().splitlines()}
             log.info(f'Found {len(self._ids)} results')
-    
+
     def __enter__(self):
         self.file = gzip.open(str(self.path), 'at')
         return self
@@ -58,7 +59,7 @@ class GzipJsonpFile(Saver):
             if self._to_be_flushed == self._flush_every:
                 self.flush()
                 self._to_be_flushed = 0
-    
+
     def flush(self):
         self.file.flush()
 
@@ -66,7 +67,7 @@ class GzipJsonpFile(Saver):
 class MongoCollection(Saver):
     def __init__(
         self,
-        db_name, 
+        db_name,
         collection_name,
         host='mongo',
         port=27017,
@@ -97,7 +98,7 @@ class MongoCollection(Saver):
         self.collection = None
         self.client.close()
         self.client = None
-    
+
     def __contains__(self, _id):
         assert self.collection is not None, "cannot use MongoCollection outside a 'with' statement"
         return self.collection.find_one({'_id': _id}, {'_id': True}) is not None
@@ -106,7 +107,7 @@ class MongoCollection(Saver):
         self._batch.append(record)
         if len(self._batch) == self.batch_size:
             self.flush()
-    
+
     def flush(self):
         if self._batch:
             try:
@@ -115,3 +116,52 @@ class MongoCollection(Saver):
                 self._batch.clear()
             except BulkWriteError as bwe:
                 print(bwe.details)
+
+
+class HDF5File(Saver):
+    """ Save / Load / Append results in a HDF5 file. """
+    def __init__(self, path, shape=None, flush_every=100):
+        self.path = Path(path)
+        self.shape = shape
+        self.flush_every = flush_every
+
+        self._ids = dict()
+        self._to_be_flushed = 0
+
+        self._ids_dataset = None
+        self._data_dataset = None
+
+        if self.path.exists():
+            with h5py.File(str(self.path), 'r') as f:
+                self._ids = {_id.decode('utf8'): i for i, _id in enumerate(f['ids'])}
+            log.info(f'Found {len(self._ids)} results')
+
+    def __enter__(self):
+        self.file = h5py.File(str(self.path), 'a')
+        self._ids_dataset = self.file.require_dataset('ids', shape=self.shape[:1], dtype=h5py.string_dtype())
+        self._data_dataset = self.file.require_dataset('data', shape=self.shape, dtype='float32')
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file.close()
+
+    def __contains__(self, _id):
+        return _id in self._ids
+
+    def add(self, record):
+        _id = record['_id']
+        if _id in self:
+            return            
+
+        new_index = len(self._ids)
+        self._ids[_id] = new_index
+        self._ids_dataset[new_index] = _id
+        self._data_dataset[new_index, :] = record['feature']
+
+        self._to_be_flushed += 1
+        if self._to_be_flushed == self.flush_every:
+            self.flush()
+            self._to_be_flushed = 0
+
+    def flush(self):
+        self.file.flush()
