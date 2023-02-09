8f264b929b17f6439907865c282660bed180c48a
Fabio Carrara <fabio.carrara@isti.cnr.it>
2023-02-09 14:49:07 +0100

    restructured project for packaging

--- visione/cli/__main__.py
+++ visione/cli/__main__.py
@@ -7,7 +7,7 @@ import sys
 from . import commands
 
 
-if __name__ == "__main__":
+def main():
     install_dir = Path(__file__).resolve(strict=True).parent.parent  # installation dir of VISIONE
     collection_dir = Path.cwd()  # data dir of current collection
     cache_dir = os.environ.get("XDG_CACHE_HOME", None) or Path.home() / '.cache'
@@ -31,4 +31,8 @@ if __name__ == "__main__":
     args = vars(args)
     func = args.pop('func')
     ret = func(**args)
-    sys.exit(ret)
+    return ret
+
+
+if __name__ == "__main__":
+    sys.exit(main())
