01bebee5ba01166e3b2e6359ed21eda0572cb49b
Fabio Carrara <fabio.carrara@isti.cnr.it>
2023-02-10 16:46:56 +0100

    cli: added build command

--- visione/cli/commands/__init__.py
+++ visione/cli/commands/__init__.py
@@ -1,9 +1,11 @@
+from .build import BuildCommand
 from .import_ import ImportCommand
 from .index import IndexCommand
 from .init import InitCommand
 from .serve import ServeCommand
 
 available_commands = (
+    BuildCommand,
     ImportCommand,
     IndexCommand,
     InitCommand,
---
+++ visione/cli/commands/build.py
@@ -0,0 +1,23 @@
+import importlib.resources
+from pathlib import Path
+import shutil
+import subprocess
+
+from .command import BaseCommand
+
+
+class BuildCommand(BaseCommand):
+    """ Implements the 'build' CLI command. """
+
+    def __init__(self, *args, **kwargs):
+        super(BuildCommand, self).__init__(*args, **kwargs)
+    
+    def add_arguments(self, subparsers):
+        parser = subparsers.add_parser('build', help='Build or rebuild services.')
+        parser.add_argument('services', nargs='+', help='services to (re)build (defaults to all)')
+        parser.set_defaults(func=self)
+    
+    def __call__(self, *, services):
+        command = self.compose_cmd + ['build'] + services
+        ret = subprocess.run(command, check=True, env=self.visione_env)
+        return ret
