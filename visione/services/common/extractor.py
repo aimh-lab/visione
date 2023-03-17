0b53a4f639b38eef522fbd8bdf4d5b803d3944d0
Fabio Carrara <fabio.carrara@isti.cnr.it>
2023-03-17 18:36:24 +0100

    common: impoved base extractor progress

--- visione/services/common/extractor.py
+++ visione/services/common/extractor.py
@@ -1,8 +1,9 @@
+import csv
 from pathlib import Path
 import itertools
 import more_itertools
 
-from visione import cli_progress
+from visione import CliProgress
 from visione.savers import GzipJsonlFile, HDF5File
 
 
@@ -12,10 +13,10 @@ class BaseExtractor(object):
     @classmethod
     def add_arguments(cls, parser):
         """ Add arguments to the parser. """
-        parser.add_argument('--batch-size', type=int, default=3, help='batch size')
+        parser.add_argument('--batch-size', type=int, help='batch size')
         parser.add_argument('--force', default=False, action='store_true', help='overwrite existing data')
         parser.add_argument('--gpu', default=False, action='store_true', help='use the GPU if available')
-        parser.add_argument('--save-every', type=int, default=100, help='save every N records')
+        parser.add_argument('--save-every', type=int, default=5000, help='save every N records')
 
         parser.add_argument('input_images', type=Path, help='images to be processed.'
             'Can be a directory or a file with a list of images.'
@@ -36,6 +37,7 @@ class BaseExtractor(object):
         """ Initialize the extractor. """
         super(BaseExtractor, self).__init__()
         self.args = args
+        self.args.batch_size = self.args.batch_size or self.args.save_every
 
     def parse_input(self):
         """ Parses the input file and returns a list of (video_id, frame_id, frame_path) tuples. """
@@ -66,6 +68,7 @@ class BaseExtractor(object):
     def get_saver(self, video_id):
         """ Returns a saver for the given video id. """
         output_path = str(self.args.output).format(video_id=video_id)
+        Path(output_path).parent.mkdir(parents=True, exist_ok=True)
         save_every = self.args.save_every
 
         if self.args.output_type == 'jsonl':
@@ -78,29 +81,45 @@ class BaseExtractor(object):
         """ Loads a batch of images and extracts features. """
         raise NotImplementedError()
 
+    def skip_existing(self, ids_and_paths, progress=None):
+        """ Skips images that have already been processed. """
+        for video_id, group in itertools.groupby(ids_and_paths, key=lambda x: x[0]):
+            with self.get_saver(video_id) as saver:
+                to_be_processed = []
+                for video_id, image_id, image_path in group:
+                    if image_id not in saver:
+                        to_be_processed.append((video_id, image_id, image_path))
+                    elif progress:
+                        progress.initial += 1
+            # ensure saver is closed before yielding from group
+            yield from to_be_processed
+
     def run(self):
         ids_and_paths = self.parse_input()
-        ids_and_paths = cli_progress(ids_and_paths, initial=0, total=len(ids_and_paths))
+        n_images = len(ids_and_paths)
+
+        progress = CliProgress(initial=0, total=n_images)
+
+        if not self.args.force:
+            ids_and_paths = self.skip_existing(ids_and_paths, progress)
+
+        # unzip ids and paths
+        ids_and_paths = more_itertools.unzip(ids_and_paths)
+        video_ids, image_ids, image_paths = more_itertools.padded(ids_and_paths, fillvalue=(), n=3)
+
+        # process images in batches
+        batched_image_paths = more_itertools.chunked(image_paths, self.args.batch_size)
+        batched_records = map(self.extract, batched_image_paths)
+        records = itertools.chain.from_iterable(batched_records)
+
+        ids_and_records = zip(video_ids, image_ids, records)
+        ids_and_records = progress(ids_and_records)
 
         # group images by video id
-        for video_id, group in itertools.groupby(ids_and_paths, key=lambda x: x[0]):
-
+        for video_id, group in itertools.groupby(ids_and_records, key=lambda x: x[0]):
             with self.get_saver(video_id) as saver:
-                # filter out images that have already been processed
-                if not self.args.force:
-                    group = filter(lambda x: x[1] not in saver, group)
-
-                # unzip ids and paths
-                group = more_itertools.unzip(group)
-                _, image_ids, image_paths = more_itertools.padded(group, fillvalue=(), n=3)
-
-                # process images in batches
-                batched_image_paths = more_itertools.chunked(image_paths, self.args.batch_size)
-                batched_records = map(self.extract, batched_image_paths)
-                records = itertools.chain.from_iterable(batched_records)
-
                 # make records
-                records = ({'_id': _id, **record} for _id, record in zip(image_ids, records))
+                records = ({'_id': _id, **record} for _, _id, record in group)
 
                 # save records
                 saver.add_many(records, force=self.args.force)
