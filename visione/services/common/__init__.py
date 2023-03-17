f43e7a7a65729532cbe94ab2807188523e2199d1
Fabio Carrara <fabio.carrara@isti.cnr.it>
2023-03-17 18:35:12 +0100

    common: refactored CLI progress into dedicated class

--- visione/services/common/__init__.py
+++ visione/services/common/__init__.py
@@ -18,16 +18,28 @@ def load_config(yaml_path):
     return config
 
 
+class CliProgress(object):
+    """ Progress bar for CLI. """
+    def __init__(self, initial=0, total=-1):
+        super(CliProgress, self).__init__()
+        self.initial = initial
+        self.total = -1 if total is None else total
+
+    def __call__(self, iterable):
+
+        def _wrapped(iterable):
+            print(f'progress: {self.initial}/{self.total}', flush=True)
+            for it in iterable:
+                yield it
+                self.initial += 1
+                print(f'progress: {self.initial}/{self.total}', flush=True)
+
+            if self.total < 0:
+                self.total = self.initial
+            print(f'progress: {self.initial}/{self.total}', flush=True)
+
+        return _wrapped(iterable)
+
+
 def cli_progress(iterable, initial=0, total=-1):
-    if total is None:
-        total = -1
-
-    print(f'progress: {initial}/{total}', flush=True)
-    for it in iterable:
-        yield it
-        initial += 1
-        print(f'progress: {initial}/{total}', flush=True)
-
-    if total < 0:
-        total = initial
-    print(f'progress: {initial}/{total}', flush=True)
+    return CliProgress(initial, total)(iterable)
