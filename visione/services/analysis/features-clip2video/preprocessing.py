8998244b3ecdf2d72fdb8af465c885d9004933b8
mesnico <nicola.messina93@gmail.com>
2023-05-08 15:56:57 +0200

    clip2video: initial pipeline for video processing

---
+++ visione/services/analysis/features-clip2video/preprocessing.py
@@ -0,0 +1,63 @@
+import os
+import subprocess
+from multiprocessing import Pool
+import csv
+import tqdm
+import shlex
+import json
+
+def extract_frames(video_name, start_time, end_time, out_file, fps=5):
+    # dur = end_time - start_time
+    if os.path.isfile(out_file) and os.path.getsize(out_file) != 0:
+        return None
+        # os.system('rm -rf ' + out_file)
+    dur = end_time - start_time
+    cmd = 'ffmpeg -y -v 0 -ss %.02f -i %s -t %.02f -r %d -q 0 -vf scale=320x240 -preset faster %s' % (start_time, video_name, dur, fps, out_file)
+    # print(cmd)
+    # os.system(cmd)
+    cmd = shlex.split(cmd)
+
+    ffmpeg = subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    out, err = ffmpeg.communicate()
+    if err:
+        print('Error in processing video {}: {}'.format(video_name, err))
+
+def process(line):
+    # print(line)
+    mp4_name, start_time, end_time, out_folder = line
+    extract_frames(mp4_name, start_time, end_time, out_folder)
+
+def preprocess_shots(shot_infos, min_duration=0.0, num_threads=5, out_folder="video_feat_extraction"):
+    """
+    Preprocesses the shot metadata and performs ffmpeg processing.
+    It includes merging together the shots if they are too short, if needed.
+
+    shot_info: tuple containing video_id, shot_id, video_path, start_frame, start_time, end_frame, end_time
+    min_duration: used for merging shots if duration is less than this amount
+    """
+
+    # FIXME: if min_duration > 0 (merging enabled), we first need to aggregate the tuples by video_id
+    # Otherwise, there's the risk that shots from different videos are merged together
+    if min_duration > 0.0:
+        raise NotImplementedError()
+
+    lines = []
+    skip = False
+    for video_id, shot_id, video_path, start_frame, start_time, end_frame, end_time in shot_infos:
+        # merge contiguous shots if the minimum time length (min_duration) is not reached
+        start_time = start_time if not skip else start_time
+        shot_id_visione = shot_id if not skip else shot_id_visione
+        end_time = end_time
+        if end_time - start_time < min_duration:
+            skip = True
+            continue
+        else:
+            skip = False
+        # out_video_ext = '.mp4' # os.path.splitext(mp4_file)[1]
+        out_file = os.path.join(out_folder, '{}_{}.mp4'.format(video_id, shot_id_visione))
+        lines.append((video_path, start_time, end_time, out_file))
+
+    # starting parallel ffmpeg processing, multi-threaded
+    with Pool(processes=num_threads) as p:
+        for _ in tqdm.tqdm(p.imap_unordered(process, lines), total=len(lines)):
+            pass
--- visione/services/common/extractor.py
+++ visione/services/common/extractor.py
@@ -3,9 +3,10 @@ from pathlib import Path
 import itertools
 import more_itertools
 
-from visione import CliProgress
-from visione.savers import GzipJsonlFile, HDF5File
-
+# from visione import CliProgress
+# from visione.savers import GzipJsonlFile, HDF5File
+from . import CliProgress
+from .savers import GzipJsonlFile, HDF5File
 
 class BaseExtractor(object):
     """ Base class for all extractors. """
@@ -117,6 +118,126 @@ class BaseExtractor(object):
 
         # group images by video id
         for video_id, group in itertools.groupby(ids_and_records, key=lambda x: x[0]):
+            with self.get_saver(video_id) as saver:
+                # make records
+                records = ({'_id': _id, **record} for _, _id, record in group)
+
+                # save records
+                saver.add_many(records, force=self.args.force)
+
+
+class BaseVideoExtractor(object):
+    """ Base class for all video extractors. """
+
+    @classmethod
+    def add_arguments(cls, parser):
+        """ Add arguments to the parser. """
+        parser.add_argument('--batch-size', type=int, help='batch size')
+        parser.add_argument('--force', default=False, action='store_true', help='overwrite existing data')
+        parser.add_argument('--gpu', default=False, action='store_true', help='use the GPU if available')
+        parser.add_argument('--save-every', type=int, default=5000, help='save every N records')
+
+        parser.add_argument('input_shots', type=Path, help='file containing the detected scenes.'
+            'It is a file containing scenes information, in the format '
+            '[Scene Number,Start Frame,Start Timecode,Start Time (seconds),End Frame,End Timecode,End Time (seconds),Length (frames),Length (timecode),Length (seconds)]'
+            )
+
+        subparsers = parser.add_subparsers(dest='output_type')
+
+        file_parser = subparsers.add_parser('jsonl', help='save results to gzipped JSONL files')
+        file_parser.add_argument('-o', '--output', type=Path, help='output path template, where "{video_id}" will be replaced by the video id.')
+
+        hdf5_parser = subparsers.add_parser('hdf5', help='save results to HDF5 files')
+        hdf5_parser.add_argument('-n', '--features-name', default='generic', help='identifier of feature type')
+        hdf5_parser.add_argument('-o', '--output', type=Path, help='output path template, where "{video_id}" will be replaced by the video id.')
+
+    def __init__(self, args):
+        """ Initialize the extractor. """
+        super(BaseVideoExtractor, self).__init__()
+        self.args = args
+        self.args.batch_size = self.args.batch_size or self.args.save_every
+
+    def parse_input(self):
+        """ Parses the input file and returns a list of 
+        (video_id, shot_id, video_path, start_frame, start_time, end_frame, end_time) tuples. """
+        input_shots = self.args.input_shots
+        if input_shots.is_dir():
+            video_id = input_shots.stem
+            scenes_file = self.args.input_shots / f'{video_id}-scenes.csv'
+            assert scenes_file.is_file()  # input should be a file, parse it
+            video_path = input_shots.parents[1] / 'videos' / f'{video_id}.mp4'
+        else:
+            raise ValueError()
+
+        with scenes_file.open() as f:
+            reader = csv.reader(f)
+
+            # peek at the first line to determine the number of columns
+            peek, reader = itertools.tee(reader)
+            num_cols = len(next(peek))
+            del peek
+
+            assert num_cols == 10
+
+            # parse the rest of the file
+            parse_row = lambda row: (video_id, row[0], video_path, row[1], row[3], row[4], row[6])
+
+            shot_paths_and_times = [parse_row(row) for row in reader]
+
+        return shot_paths_and_times
+
+    def get_saver(self, video_id):
+        """ Returns a saver for the given video id. """
+        output_path = str(self.args.output).format(video_id=video_id)
+        Path(output_path).parent.mkdir(parents=True, exist_ok=True)
+        save_every = self.args.save_every
+
+        if self.args.output_type == 'jsonl':
+            return GzipJsonlFile(output_path, flush_every=save_every)
+
+        elif self.args.output_type == 'hdf5':
+            return HDF5File(output_path, flush_every=save_every, attrs={'features_name': self.args.features_name})
+
+    def extract(self, shot_info):
+        """ Loads a batch of shots and extracts features. """
+        raise NotImplementedError()
+
+    def skip_existing(self, shot_paths_and_times, progress=None):
+        """ Skips shots that have already been processed. """
+        for video_id, group in itertools.groupby(shot_paths_and_times, key=lambda x: x[0]):
+            with self.get_saver(video_id) as saver:
+                to_be_processed = []
+                for video_id, shot_id, image_path, *_ in group:
+                    if shot_id not in saver:
+                        to_be_processed.append((video_id, shot_id, image_path))
+                    elif progress:
+                        progress.initial += 1
+            # ensure saver is closed before yielding from group
+            yield from to_be_processed
+
+    def run(self):
+        shot_paths_and_times = self.parse_input()
+        n_shots = len(shot_paths_and_times)
+
+        progress = CliProgress(initial=0, total=n_shots)
+
+        if not self.args.force:
+            shot_paths_and_times = self.skip_existing(shot_paths_and_times, progress)
+
+        # process images in batches
+        batched_image_paths = more_itertools.chunked(shot_paths_and_times, self.args.batch_size)
+        batched_records = map(self.extract, batched_image_paths)
+        records = itertools.chain.from_iterable(batched_records)
+
+        # unzip ids and paths
+        shot_paths_and_times_unzipped = more_itertools.unzip(shot_paths_and_times)
+        video_ids, image_ids, image_paths = shot_paths_and_times_unzipped
+
+        shot_ids_and_records = zip(video_ids, image_ids, records)
+        shot_ids_and_records = progress(shot_ids_and_records)
+
+        # group images by video id
+        for video_id, group in itertools.groupby(shot_ids_and_records, key=lambda x: x[0]):
             with self.get_saver(video_id) as saver:
                 # make records
                 records = ({'_id': _id, **record} for _, _id, record in group)
