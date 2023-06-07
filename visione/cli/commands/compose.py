7fc20d5647ecb1f5689d746600ee62441274cff3
Fabio Carrara <fabio.carrara@isti.cnr.it>
2023-06-07 19:33:19 +0200

    cli: compose: added config-based profiles

--- visione/cli/commands/compose.py
+++ visione/cli/commands/compose.py
@@ -18,6 +18,16 @@ class ComposeCommand(BaseCommand):
     def __call__(self, *, cmd, **kwargs):
         super(ComposeCommand, ComposeCommand).__call__(self, **kwargs)
 
-        command = self.compose_cmd + cmd
+        # selects mandatory services needed to respond to queries
+        profile_options = ['--profile', 'query']
+
+        # figure out which optional services are also needed;
+        # each optional service is contained in a profile named as the service
+        enabled_features = self.config.get('analysis', {}).get('features', [])
+        optional_services = [f'features-{f}' for f in enabled_features]
+        for service in optional_services:
+            profile_options.extend(['--profile', service])
+
+        command = self.compose_cmd + profile_options + cmd
         ret = subprocess.check_call(command, env=self.compose_env)
         return ret
