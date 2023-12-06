ec1d3bcb63f817342225d7b21ff3105206752bd4
Fabio Carrara <fabio.carrara@isti.cnr.it>
2023-12-06 17:13:08 +0000

    added open-clip extractors and biomed clip service

---
+++ visione/services/analysis/features-openclip/extract.py
@@ -0,0 +1,113 @@
+import argparse
+import itertools
+import logging
+import os
+
+import more_itertools
+from PIL import Image
+import torch
+import open_clip
+
+from visione.extractor import BaseExtractor
+
+
+loggers = [logging.getLogger(name) for name in logging.root.manager.loggerDict]
+for logger in loggers:
+    logger.setLevel(logging.WARNING)
+
+
+class ImageListDataset(torch.utils.data.Dataset):
+    def __init__(self, paths, processor):
+        self.paths = paths
+        self.processor = processor
+    
+    def __len__(self):
+        return len(self.paths)
+    
+    def __getitem__(self, idx):
+        path = self.paths[idx]
+        image = Image.open(path)
+        image_pt = self.processor(image)
+        return image_pt
+
+
+class WrapIterableDataset(torch.utils.data.IterableDataset):
+    def __init__(self, iterable, batch_size, processor, preload=False):
+        self.iterable = iterable
+        self.batch_size = batch_size
+        self.processor = processor
+
+        if preload:
+            self.iterable = list(self.iterable)
+    
+    def process(self, item):
+        images = [Image.open(i) for i in item]
+        images_pt = self.processor(images=images, return_tensors="pt")
+        return images_pt
+  
+    def __iter__(self):
+        worker_info = torch.utils.data.get_worker_info()
+
+        itr = self.iterable
+        if worker_info is not None:
+            itr = itertools.islice(self.iterable, worker_info.id, None, worker_info.num_workers)
+        
+        itr = more_itertools.chunked(itr, self.batch_size)
+        itr = map(self.process, itr)
+        yield from itr
+
+
+class OpenCLIPExtractor(BaseExtractor):
+
+    @classmethod
+    def add_arguments(cls, parser):
+        parser.add_argument('--model-handle', default=os.environ['MODEL_HANDLE'], help='model handle')
+        parser.add_argument('--batch-size', default=1, type=int, help='batch size')
+        parser.add_argument('--num-workers', default=4, type=int, help='number of workers')
+        super(OpenCLIPExtractor, cls).add_arguments(parser)
+
+    def __init__(self, args):
+        super(OpenCLIPExtractor, self).__init__(args)
+        self.device = None
+        self.model = None
+        self.processor = None
+
+    def setup(self):
+        if self.model is None:
+            # lazy load models
+            self.device = 'cuda' if self.args.gpu and torch.cuda.is_available() else 'cpu'
+            os.makedirs('/cache/open_clip', exist_ok=True)
+            self.model, _, self.processor = open_clip.create_model_and_transforms(self.args.model_handle, cache_dir='/cache/open_clip')
+            self.model.to(self.device)
+            self.model.eval()
+
+    def extract(self, image_paths):
+        records = list(self.extract_iterable(image_paths))
+        return records
+
+    def extract_iterable(self, image_paths):
+        self.setup()  # lazy load model
+
+        # FIXME: iterable dataset has problems with h5py in multiprocessing, it only works with preload=True
+        # dataset = WrapIterableDataset(image_paths, batch_size, self.processor, preload=True)
+        # dataloader = torch.utils.data.DataLoader(dataset, batch_size=None, num_workers=24)
+
+        # if we must preload, we might as well use a standard dataset
+        image_paths = list(image_paths)
+        dataset = ImageListDataset(image_paths, self.processor)
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size=self.args.batch_size, num_workers=self.args.num_workers)
+        
+        with torch.no_grad():
+            for images_pt in dataloader:
+                images_pt = images_pt.to(self.device)
+                images_features = self.model.encode_image(images_pt).float()
+                records = [{'feature_vector': f.tolist()} for f in images_features.cpu().numpy()]
+                yield from records
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Extract features from an Open CLIP model')
+    OpenCLIPExtractor.add_arguments(parser)
+    args = parser.parse_args()
+    extractor = OpenCLIPExtractor(args)
+    extractor.run()
---
+++ visione/services/analysis/features-openclip/service.py
@@ -0,0 +1,91 @@
+import argparse
+import logging
+import os
+
+from flask import Flask, request, jsonify
+import requests
+import torch
+from torch.nn import functional as F
+import open_clip
+
+
+# setup logging
+logging.basicConfig(level=logging.DEBUG)
+
+# create the Flask app
+app = Flask(__name__)
+
+
+class OpenCLIPTextEncoder():
+    def __init__(self, model_handle):
+        device = 'cpu'
+        self.model, _, _ = open_clip.create_model_and_transforms(model_handle)
+        self.tokenizer = open_clip.get_tokenizer(model_handle)
+
+    def get_text_embedding(self, text, normalized=False):
+        with torch.no_grad():
+            inputs = self.tokenizer(text, context_length=256)
+            text_features = self.model.encode_text(inputs)
+            if normalized:
+                text_features = F.normalize(text_features, dim=-1)
+            text_features = text_features.numpy()
+        return text_features
+
+
+@app.route('/get-text-feature', methods=['GET'])
+def get_text_features():
+    text = request.args.get("text")
+    logging.info('Received text: {}'.format(text))
+    text_feature = qe.get_text_embedding(text, normalized=args.normalized)
+    out = jsonify(text_feature.tolist())
+    return out
+
+# deprecated, kept for backward compatibility of 'core' service
+@app.route('/text-to-image-search', methods=['GET'])
+def text_to_image_search():
+    text = request.args.get("text")
+    k = request.args.get("k", type=int, default=10000)
+    logging.info('Received text: {}'.format(text))
+    text_feature = qe.get_text_embedding(text, normalized=args.normalized)
+
+    response = requests.post('http://faiss-index-manager:4010/search', json={
+        'type': features_name,
+        'feature_vector': text_feature.tolist(),
+        'k': k,
+    }).content
+
+    return response
+
+# deprecated, kept for backward compatibility of 'core' service
+@app.route('/internal-image-search', methods=['GET'])
+def internal_image_search():
+    img_id = request.args.get("imgId")
+    k = request.args.get("k", type=int, default=10000)
+
+    response = requests.post('http://faiss-index-manager:4010/search', json={
+        'type': features_name,
+        'query_id': img_id,
+        'k': k,
+    }).content
+
+    return response
+
+
+if __name__ == '__main__':
+    default_model_handle = os.environ['MODEL_HANDLE']
+    features_name = os.environ['FEATURES_NAME']
+
+    parser = argparse.ArgumentParser(description='Service for query feature extraction for Open CLIP models.')
+
+    parser.add_argument('--host', default='0.0.0.0', help="IP address to use for binding")
+    parser.add_argument('--port', default='5030', help="Port to use for binding")
+    parser.add_argument('--model-handle', default=default_model_handle, help='hugging face handle of the Open CLIP model')
+    parser.add_argument('--no-normalized', action='store_false', dest='normalized', default=True, help='Whether to normalize features or not')
+
+    args = parser.parse_args()
+
+    # init the query encoder
+    qe = OpenCLIPTextEncoder(args.model_handle)
+
+    # run the flask app
+    app.run(debug=False, host=args.host, port=args.port)
