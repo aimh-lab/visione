8ee9a1b4072de792037efdee279691fb892b28ac
paolo.bolettieri <paolo.bolettieri@isti.cnr.it>
2026-02-09 18:00:34 +0100

    Svelte-kit config files

---
+++ visioneUI/vite.config.ts
@@ -0,0 +1,24 @@
+import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(), 
+    sveltekit(), 
+    mkcert({
+      hosts: ['localhost', 'tuo-hostname.local', '192.168.x.x']
+    })
+  ],
+  server: { 
+    https: true,
+    host: true  // o '0.0.0.0' per accettare connessioni da tutti gli indirizzi
+  },
+  resolve: {
+    alias: {
+      '$stores': '/src/stores',
+    }
+  }
+});
+
